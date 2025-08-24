042597ea02a0d878eb96f0cb70f9b2f8f42d0f53
LucasGGamerM <LucasGGamerM@protonmail.com>
2025-08-24 16:39:49 -0300

    feat: use vim-surround binds for mini.surround

--- lua/kickstart/plugins/mini.lua
+++ lua/kickstart/plugins/mini.lua
@@ -11,11 +11,30 @@ return {
       require('mini.ai').setup { n_lines = 500 }
 
       -- Add/delete/replace surroundings (brackets, quotes, etc.)
-      --
-      -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-      -- - sd'   - [S]urround [D]elete [']quotes
-      -- - sr)'  - [S]urround [R]eplace [)] [']
-      require('mini.surround').setup()
+      require('mini.surround').setup {
+        custom_surroundings = {
+          ['('] = { output = { left = '( ', right = ' )' } },
+          ['['] = { output = { left = '[ ', right = ' ]' } },
+          ['{'] = { output = { left = '{ ', right = ' }' } },
+          ['<'] = { output = { left = '< ', right = ' >' } },
+        },
+        mappings = {
+          add = 'ys',
+          delete = 'ds',
+          find = '',
+          find_left = '',
+          highlight = '',
+          replace = 'cs',
+          update_n_lines = '',
+        },
+        search_method = 'cover_or_next',
+      }
+
+      -- Remap adding surrounding to Visual mode selection
+      vim.api.nvim_set_keymap('x', 'S', [[:<C-u>lua MiniSurround.add('visual')<CR>]], { noremap = true })
+
+      -- Make special mapping for "add surrounding for line"
+      vim.api.nvim_set_keymap('n', 'yss', 'ys_', { noremap = false })
 
       -- Simple and easy statusline.
       --  You could remove this setup call if you don't like it,
